5832d38f22d30b94149c2242632de16840d9fc0a
kozlovartemq <93704133+kozlovartemq@users.noreply.github.com>
2021-11-15 11:02:08 +0700

    Create Solution.sql

---
+++ sql/mySQL/Advanced Select/New Companies/Solution.sql
@@ -0,0 +1,12 @@
+SELECT Company.Company_code, company.founder, 
+COUNT(distinct LEAD_MANAGER.LEAD_MANAGER_code), /* distinct - взаимодействие только с неповторяющимися данными */
+COUNT(distinct Senior_Manager.Senior_Manager_code),
+COUNT(distinct Manager.Manager_code),
+COUNT(distinct Employee.Employee_code)
+FROM COMPANY, LEAD_MANAGER, Senior_Manager, Manager, Employee 
+WHERE   Company.COMPANY_code = Lead_Manager.COMPANY_code and
+        Company.COMPANY_code = Senior_Manager.COMPANY_code and
+        Company.COMPANY_code = Manager.COMPANY_code and
+        Company.COMPANY_code = Employee.COMPANY_code
+group by Company.COMPANY_code, company.founder
+order by Company.COMPANY_code 
