08f0656a465d2b9fc1b90dfb7354347ad101a495
kozlovartemq <93704133+kozlovartemq@users.noreply.github.com>
2021-11-15 15:31:56 +0700

    Update Solution.sql

--- sql/mySQL/Basic Select/Weather Observation Station 5/Solution.sql
+++ sql/mySQL/Basic Select/Weather Observation Station 5/Solution.sql
@@ -1,4 +1,4 @@
 select city, length(city) from STATION /* length() - func that counts the number of characters */
-order by length(city), city limit 1;   /* limit - the nimber of the rows to output
+order by length(city), city limit 1;   /* limit - the nimber of the rows to output */
 select city, length(city) from STATION
-order by length(city) desc, city asc limit 1; /* desc - descending, asc - ascending
+order by length(city) desc, city asc limit 1; /* desc - descending, asc - ascending */
