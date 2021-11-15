845fa4228b3c14498a7528763e386284963ee6d8
kozlovartemq <93704133+kozlovartemq@users.noreply.github.com>
2021-11-15 15:25:21 +0700

    Update Solution.sql

--- sql/mySQL/Advanced Select/Binary Tree Nodes/Solution.sql
+++ sql/mySQL/Advanced Select/Binary Tree Nodes/Solution.sql
@@ -2,7 +2,7 @@ SELECT N,
 CASE 
     WHEN p IS null
         THEN 'Root'
-    WHEN N IN (SELECT DISTINCT P from BST) /* SELECT DISTINCT - выбор неповторяющихся данных */
+    WHEN N IN (SELECT DISTINCT P from BST) /* SELECT DISTINCT - interact only with non-duplicate data */
         THEN 'Inner'
     ELSE 'Leaf'
 END
