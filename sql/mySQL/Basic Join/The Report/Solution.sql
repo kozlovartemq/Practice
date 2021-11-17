066cc68c4b22359237f664e471eb237fc723877d
kozlovartemq <93704133+kozlovartemq@users.noreply.github.com>
2021-11-17 20:30:12 +0700

    Update Solution.sql

--- sql/mySQL/Basic Join/The Report/Solution.sql
+++ sql/mySQL/Basic Join/The Report/Solution.sql
@@ -1,10 +1,10 @@
 select 
 case 
-    WHEN Grades.Grade >= 8                   /* IF could have been used instead of CASE: 
-    THEN Students.Name                          IF(Grades.Grade >= 8, Students.Name, null)  */
+    WHEN Grades.Grade >= 8                   /* IF could have been used instead of CASE:    */
+    THEN Students.Name                       /* IF(Grades.Grade >= 8, Students.Name, null)  */
 END AS Names,
 Grades.Grade, Students.Marks from Students
 Inner Join Grades                             
-ON Students.Marks >= Grades.MIN_Mark AND      /* BETWEEN could have been used: 
-    Students.Marks <= Grades.MAX_Mark            BETWEEN Grades.MIN_Mark AND Grades.MAX_Mark */
+ON  Students.Marks >= Grades.MIN_Mark AND     /* BETWEEN could have been used:               */
+    Students.Marks <= Grades.MAX_Mark         /* BETWEEN Grades.MIN_Mark AND Grades.MAX_Mark */
 order by Grades.Grade desc, Names asc
