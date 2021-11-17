a6dd5cfa1fd6595c431504f2184dd5002edd8250
kozlovartemq <93704133+kozlovartemq@users.noreply.github.com>
2021-11-17 20:27:41 +0700

    Update Solution.sql

--- sql/mySQL/Basic Join/Challenges/Solution.sql
+++ sql/mySQL/Basic Join/Challenges/Solution.sql
@@ -1,52 +1,52 @@
 select RES_TABLE.id, RES_TABLE.n, RES_TABLE.res
 FROM
-(
-SELECT h.hacker_id AS id, h.name AS n, count(ch.hacker_id) AS res
-FROM Hackers AS h INNER JOIN Challenges AS ch
-ON h.hacker_id = ch.hacker_id
-group by ch.hacker_id, h.name
-order by res desc
-) AS RES_TABLE
+        (
+        SELECT h.hacker_id AS id, h.name AS n, count(ch.hacker_id) AS res
+        FROM Hackers AS h INNER JOIN Challenges AS ch
+        ON h.hacker_id = ch.hacker_id
+        group by ch.hacker_id, h.name
+        order by res desc
+        ) AS RES_TABLE                                                        /* RES_TABLE, tablee - The table with unfiltered results */
 INNER JOIN
-(
-select tablee.res AS ress, count(tablee.res) AS ReplyCOUNT
-FROM
-(
-SELECT h.hacker_id, h.name, count(ch.hacker_id) AS res
-FROM Hackers AS h INNER JOIN Challenges AS ch
-ON h.hacker_id = ch.hacker_id
-group by ch.hacker_id, h.name
-order by res desc
-) AS tablee
-group by tablee.res 
-) AS RE_TABLE
+        (
+        select tablee.res AS ress, count(tablee.res) AS ReplyCOUNT
+        FROM
+                (
+                SELECT h.hacker_id, h.name, count(ch.hacker_id) AS res
+                FROM Hackers AS h INNER JOIN Challenges AS ch
+                ON h.hacker_id = ch.hacker_id
+                group by ch.hacker_id, h.name
+                order by res desc
+                ) AS tablee
+        group by tablee.res 
+        ) AS RE_TABLE                                                         /* RES_TABLE - The table contains a match of repeated rows with the same values of count(ch.hacker_id)*/
 ON RE_TABLE.ress = RES_TABLE.res
 WHERE
 RES_TABLE.res = 
         (
         select max(RES_TABLE1.res)
         FROM
-        (
-            SELECT h.hacker_id AS id, h.name, count(ch.hacker_id) AS res
-            FROM Hackers AS h INNER JOIN Challenges AS ch
-            ON h.hacker_id = ch.hacker_id
-            group by ch.hacker_id, h.name
-            order by res desc
-         )  AS RES_TABLE1
-         )
+                (
+                SELECT h.hacker_id AS id, h.name, count(ch.hacker_id) AS res
+                FROM Hackers AS h INNER JOIN Challenges AS ch
+                ON h.hacker_id = ch.hacker_id
+                group by ch.hacker_id, h.name
+                order by res desc
+                ) AS RES_TABLE1
+                )
 OR
 (
 RES_TABLE.res != 
         (
         select max(RES_TABLE1.res)
         FROM
-        (
-            SELECT h.hacker_id AS id, h.name, count(ch.hacker_id) AS res
-            FROM Hackers AS h INNER JOIN Challenges AS ch
-            ON h.hacker_id = ch.hacker_id
-            group by ch.hacker_id, h.name
-            order by res desc
-         )  AS RES_TABLE1
+                (
+                SELECT h.hacker_id AS id, h.name, count(ch.hacker_id) AS res
+                FROM Hackers AS h INNER JOIN Challenges AS ch
+                ON h.hacker_id = ch.hacker_id
+                group by ch.hacker_id, h.name
+                order by res desc
+                ) AS RES_TABLE1
          )
 AND ReplyCOUNT = 1
 )
