d9a6f21f29f8f4284b747bcbe714977eb1ab80bc
kozlovartemq <93704133+kozlovartemq@users.noreply.github.com>
2022-01-09 19:02:56 +0700

    Update Solution.py

--- Python3/Built-Ins/Roman to integer/Solution.py
+++ Python3/Built-Ins/Roman to integer/Solution.py
@@ -13,35 +13,35 @@ class Solution:
             if s[i] == "I" and s[i+1] == "V":
                 add_wrong_indexes(i)
                 sum_array.append(4)
-            if s[i] == "I" and s[i+1] == "X":
+            elif s[i] == "I" and s[i+1] == "X":
                 add_wrong_indexes(i)
                 sum_array.append(9)
-            if s[i] == "X" and s[i+1] == "L":
+            elif s[i] == "X" and s[i+1] == "L":
                 add_wrong_indexes(i)
                 sum_array.append(40)
-            if s[i] == "X" and s[i+1] == "C":
+            elif s[i] == "X" and s[i+1] == "C":
                 add_wrong_indexes(i)
                 sum_array.append(90)
-            if s[i] == "C" and s[i+1] == "D":
+            elif s[i] == "C" and s[i+1] == "D":
                 add_wrong_indexes(i)
                 sum_array.append(400)
-            if s[i] == "C" and s[i+1] == "M":
+            elif s[i] == "C" and s[i+1] == "M":
                 add_wrong_indexes(i)
                 sum_array.append(900)
         for i in [indx for indx in range(len(s)) if indx not in index_array]: #  finding the "one-element" numbers
             if s[i] == "I":
                 sum_array.append(1)
-            if s[i] == "V":
+            elif s[i] == "V":
                 sum_array.append(5)
-            if s[i] == "X":
+            elif s[i] == "X":
                 sum_array.append(10)
-            if s[i] == "L":
+            elif s[i] == "L":
                 sum_array.append(50)
-            if s[i] == "C":
+            elif s[i] == "C":
                 sum_array.append(100)
-            if s[i] == "D":
+            elif s[i] == "D":
                 sum_array.append(500)
-            if s[i] == "M":
+            elif s[i] == "M":
                 sum_array.append(1000) 
         return sum(sum_array)
                 
